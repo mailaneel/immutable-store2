043a38a598516c2d40a51527a893f7ff6480d70d
Anil G <mailaneel@gmail.com>
2015-06-09 17:42:22 +1000

    removed decorators

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 module.exports = {
     Store: require('./lib/store'),
-    CollectionClass: require('./lib/collection'), // only use this for dev or extension
-    ModelClass: require('./lib/model'),
+    Collection: require('./lib/collection'), // only use this for dev or extension
+    Model: require('./lib/model'),
     LocalStorageAware: require('./lib/decorators/local-store'),
     Queryable: require('./lib/decorators/queryable'),
-    Collection: require('./lib/decorators/collection'),
-    Model: require('./lib/decorators/model')
 };
