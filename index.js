11793b01a05cde4ad97bdb11f382bc601dfea7df
Anil G <mailaneel@gmail.com>
2015-07-21 18:13:00 +1000

    removed unused references

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = {
     Store: require('./lib/store'),
     Collection: require('./lib/collection'), // only use this for dev or extension
-    Model: require('./lib/model'),
-    LocalStorageAware: require('./lib/decorators/local-store'),
-    QueryMixin: require('./lib/query_mixin')
+    Model: require('./lib/model')
 };
